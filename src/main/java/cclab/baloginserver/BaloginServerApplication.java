e4c649f62cf9f995be418025651f2d07132a9b42
giyomdabin <dabinjang01@gmail.com>
2024-10-12 14:04:29 +0900

    add: first commit

---
+++ src/main/java/cclab/baloginserver/BaloginServerApplication.java
@@ -0,0 +1,13 @@
+package cclab.baloginserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaloginServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaloginServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cclab/baloginserver/BaloginServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cclab.baloginserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaloginServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
